dfeb830a9a801d0c28d291703a7e1f419038da58
BryhanS <80595094+BryhanS@users.noreply.github.com>
2023-05-31 20:08:55 +0000

    fixed

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,65 @@
+
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+
+let firstName = "Mitalda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// Varible name convention
+let myFirstJob = 'Coder';
+let myCurrentJob = "Teacher";
+
+let job1 = 'programmer';
+let job2 = 'tecaher';
+
+console.log(myFirstJob);
+*/
+
+/*
+
+let  javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+let age = 30;
+age= 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher'
+
+lastName = 'Salazar';
+console.log(lastName);
