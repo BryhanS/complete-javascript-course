0b409b697fb6739ddbd9f9587f1a642542bdee4d
BryhanS <bryhansalazarpacsi@gmail.com>
2023-06-01 09:16:12 -0500

    string template

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -49,7 +49,7 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
+
 
 let age = 30;
 age= 31;
@@ -63,3 +63,93 @@ job = 'teacher'
 
 lastName = 'Salazar';
 console.log(lastName);
+
+
+
+//math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageSarah /10, 2**3);
+
+
+const firstName = 'Jonas';
+const lastName = 'Pacsi';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x*= 4; //x = x *4 = 100
+x ++;
+x--;
+x--;
+
+console.log(x);
+
+
+// comparison operators
+console.log(ageJonas>ageSarah); //>, <,>=. <=
+console.log(ageSarah >=18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+Your tasks:
+1. StoreMark'sandJohn'smassandheightinvariables
+2. CalculateboththeirBMIsusingtheformula(youcanevenimplementboth
+versions)
+3. CreateaBooleanvariable'markHigherBMI'containinginformationabout
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+
+
+// var massMarks = 78;
+// var heightMarks = 1.69;
+// var massJohn =92;
+// var heightJohn = 1.95;
+
+let massMarks = 95;
+let heightMarks = 1.88;
+let massJohn =85;
+let heightJohn = 1.76;
+
+const bmiJohn = massJohn / heightJohn **2;
+const bmiMarks = massMarks / heightMarks **2;
+
+const maxHigherBMI = bmiMarks> bmiJohn;
+
+console.log(bmiJohn, bmiMarks, maxHigherBMI)
+
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!.';
+console.log(jonas);
+
+const jonasNew = `Just a regular string ...`
+
+console.log(jonasNew)
+
+console.log(`String with \n\
+multiple \n\
+lines`);
+
+console.log(`String
+Multiplr
+lines`);
+*/
+
+
