048e21a0a4c26e4ae88b8c16b918c1f34966f4dc
BryhanS <bryhansalazarpacsi@gmail.com>
2023-06-03 08:59:05 -0500

    strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let hadDriversLicense = false;
+const passTest = true;
+
+if (passTest) hadDriversLicense  = true;
+
+if(hadDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+const if = 23;
