3ed42f48c1c03d5adabd9072c3bf6c8c35ecabf6
BryhanS <bryhansalazarpacsi@gmail.com>
2023-06-07 07:01:41 -0500

    review funtion

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -84,7 +84,6 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, 'Diego'));
 console.log(yearsUntilRetirement(1980, 'Bob'));
-*/
 
 
 //funtion calling other function
@@ -104,3 +103,28 @@ function fruitProcessor(apples, oranges){
 
 console.log(fruitProcessor(2,3));
 
+*/
+
+const calAge = function (birthYear){
+    return 2037 - birthYear;
+}
+
+
+const yearsUntilRetirement = function (birthYear, firstName){
+    const age = calAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0){
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    }else{
+        console.log(`${firstName} has alredy retired`);
+        return -1;
+    }
+
+    return retirement;
+    //return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'jonas'));
+console.log(yearsUntilRetirement(1950, 'mike'));
