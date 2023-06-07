b87300fa08506e54d01c527f7c098147fd405e53
BryhanS <bryhansalazarpacsi@gmail.com>
2023-06-07 05:49:42 -0500

    funtion

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 let hadDriversLicense = false;
 const passTest = true;
 
@@ -9,4 +10,32 @@ if(hadDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
-const if = 23;
+// const if = 23;
+*/
+//funtion
+
+function logger(){
+    console.log('My name is jonas');
+}
+//calling // runing / invokung function
+logger();
+logger();
+logger();
+logger();
+
+function fullProcessor(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fullProcessor(5, 0);
+console.log(appleJuice);
+console.log(fullProcessor(5, 0));
+
+
+const appleOrangeJuice = fullProcessor(2,4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+num
