1fc488b8f456257e54c480dd33d35027ff8cf7ea
BryhanS <bryhansalazarpacsi@gmail.com>
2023-06-10 00:17:43 -0500

    basic array operations

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -195,4 +195,43 @@ const ages = [calAge(years[0]), calAge(years[1]), calAge(years[years.length -1])
 console.log(ages);
 
 
+
+//add element
+const friends = ['Michael', 'Steven', 'Peter'];
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+
+
+//remove element
+friends.pop(); // last
+
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+
+friends.shift(); // first
+console.log(friends);
+
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+console.log(friends.includes())
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('23'));
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Peter');
+}
+
 */
+
